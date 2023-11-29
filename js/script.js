441aec41e257906549fef67a26f54b5632d7bbe8
AimanFadil <aimanbello2001@gmail.com>
2023-11-29 15:45:21 +0100

    correzione pari e dispari

--- js/script.js
+++ js/script.js
@@ -1,25 +1,31 @@
 //PARI E DISPARI
-let NumScelta = prompt("Scegliere pari o dispari");
 
-let UtenteNum = parseInt(prompt("Inserisci un numero da 1 a 5"));
 
-let PcNum = Math.floor(Math.random() * 5) + 1;
-
-const somma = UtenteNum + PcNum;
-
-function PariDispari(numero) {
-    return numero % 2 === 0;
+function generateRandomNumber(){
+    return Math.floor(Math.random() * 5 + 1)
 }
 
-const Pari = PariDispari(somma);
+function checkSomma(piu){
+    if(piu % 2 == 0){
+        return 'pari';
+    }
 
+    return 'dispari';
+}
 
-if (PariDispari(somma) === (NumScelta === "pari")) {
-    console.log(`L'utente ha vinto. La somma dei due numeri è ${somma}, che è pari.`);
-} 
-else if(PariDispari(somma) === isNaN) {
-    console.log(`Non hai capito il gioco!! Questo: ${somma}, non è un risultato valido`);
+let scelta = prompt('Inserisci un valore tra pari e dispari');
+
+let numero = parseInt(prompt('Inserisci un numero tra 1 e 5 '));
+
+let numero_pc = generateRandomNumber();
+
+let somma = numero + numero_pc;
+
+let risultato = checkSomma(somma)
+
+if (risultato == scelta){
+    console.log('Hai vinto')
 }
 else{
-    console.log(`Il computer ha vinto. La somma dei due numeri è ${somma}, che è dispari.`);
+    console.log('Hai Perso')
 }
